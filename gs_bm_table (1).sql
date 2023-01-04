d7659fe7472e8ce23a3fd5befac1172b8156bab2
MaikoNakase <116089127+MaikoNakase@users.noreply.github.com>
2023-01-04 22:00:59 +0900

    Add files via upload

---
+++ gs_bm_table (1).sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost
+-- 生成日時: 2023 年 1 月 04 日 13:58
+-- サーバのバージョン： 10.4.21-MariaDB
+-- PHP のバージョン: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_db3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_bm_table`
+--
+
+CREATE TABLE `gs_bm_table` (
+  `id` int(11) NOT NULL,
+  `書籍名` int(11) NOT NULL,
+  `書籍URL` int(11) NOT NULL,
+  `登録日時` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
